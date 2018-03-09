73410432c33e6272b6cf3b6597c5c50b3c131ed2
janelle6 <33079140+janelle6@users.noreply.github.com>
2018-03-09 13:52:48 -0800

    Add files via upload

---
+++ Kueck_Lab5_Xcode.swift
@@ -0,0 +1,82 @@
+//
+//  ViewController.swift
+//  Kueck_Lab5_Again
+//
+//  Created by mjk3ll3y on 3/9/18.
+//  Copyright © 2018 janelle6. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController, UISearchBarDelegate {
+    @IBOutlet weak var janelleMap: MKMapView!
+    @IBAction func searchButton(_ sender: Any)
+    {
+        let searchController = UISearchController(searchResultsController: nil)
+        searchController.searchBar.delegate = self
+        present(searchController, animated: true, completion: nil)
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        UIApplication.shared.beginIgnoringInteractionEvents()
+        
+        let activityIndicator = UIActivityIndicatorView()
+        activityIndicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.gray
+        activityIndicator.center = self.view.center
+        activityIndicator.hidesWhenStopped = true
+        activityIndicator.startAnimating ()
+        self.view.addSubview(activityIndicator)
+        
+        searchBar.resignFirstResponder()
+        dismiss(animated: true, completion: nil)
+        
+        let searchRequest = MKLocalSearchRequest()
+        searchRequest.naturalLanguageQuery = searchBar.text
+        
+        let activeSearch = MKLocalSearch(request: searchRequest)
+        
+        activeSearch.start { (response, error) in
+            activityIndicator.stopAnimating()
+            UIApplication.shared.endIgnoringInteractionEvents()
+            if response == nil
+            {
+                print ("ERROR")
+            }
+            else
+            {
+                let annotations = self.janelleMap.annotations
+                self.janelleMap.removeAnnotations(annotations)
+                
+                let latitude = response?.boundingRegion.center.latitude
+                let longitude = response?.boundingRegion.center.longitude
+                
+                let annotation = MKPointAnnotation()
+                annotation.title = searchBar.text
+                annotation.coordinate = CLLocationCoordinate2DMake(latitude!, longitude!)
+                self.janelleMap.addAnnotation(annotation)
+                
+                let coordinates:CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude!, longitude!)
+                let span = MKCoordinateSpanMake(0.1, 0.1)
+                let region = MKCoordinateRegionMake(coordinates, span)
+                self.janelleMap.setRegion(region, animated: true)
+                
+                
+                
+            }
+        }
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
